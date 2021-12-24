fa5c911fd3f98f13afc497f858b2735c3110e048
NazarVorotsianko <nazarbatl8@gmail.com>
2021-12-24 17:31:38 +0200

    Gradle setup, Add Fonts, Colors

--- app/src/main/java/com/example/apppokedex/ui/theme/Color.kt
+++ app/src/main/java/com/example/apppokedex/ui/theme/Color.kt
@@ -2,7 +2,34 @@ package com.example.apppokedex.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val LightBlue = Color(0xFFBAC7FF)
+val LightGray = Color(0xFFAAAAAA)
+
+
+//Colors to Stats Pokemon
+val HpColor = Color(0xFFFF0000)
+val AtColor = Color(0xFFF08030)
+val DefColor = Color(0xFFF8D030)
+val SpecAtColor = Color(0xFF6890F0)
+val SpecDefColor = Color(0xFF78C850)
+val SpeedColor = Color(0xFFF85888)
+
+//Color to Type Pokemon
+val TypeNormal = Color(0xFFA8A878)
+val TypeFire = Color(0xFFF08030)
+val TypeFighting = Color(0xFFC03028)
+val TypeWater = Color(0xFF6890F0)
+val TypeFlying = Color(0xFFA890F0)
+val TypeGrass = Color(0xFF78C850)
+val TypePoison = Color(0xFFA040A0)
+val TypeElectric = Color(0xFFF8D030)
+val TypeGround = Color(0xFFE0C068)
+val TypePsychic = Color(0xFFF85888)
+val TypeRock = Color(0xFFB8A038)
+val TypeIce = Color(0xFF98D8D8)
+val TypeBug = Color(0xFFA8B820)
+val TypeDragon = Color(0xFF7038F8)
+val TypeGhost = Color(0xFF705898)
+val TypeDark = Color(0xFF705848)
+val TypeSteel = Color(0xFFB8B8D0)
+val TypeFairy = Color(0xFFEE99AC)
--- app/src/main/java/com/example/apppokedex/ui/theme/Theme.kt
+++ app/src/main/java/com/example/apppokedex/ui/theme/Theme.kt
@@ -5,17 +5,16 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color.Blue,
+    background = LightBlue,
+    onBackground = Color.Black,
+    surface = Color.White,
+    onSurface = Color.Black
 
     /* Other default colors to override
     background = Color.White,
@@ -29,14 +28,9 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun AppPokedexTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
 
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
