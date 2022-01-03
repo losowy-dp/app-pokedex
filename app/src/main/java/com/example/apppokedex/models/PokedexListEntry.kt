721ceb1f22a8b460791b4021797cf4f1c23478b2
Dmytro Podeiko <45311853+losowy-dp@users.noreply.github.com>
2022-01-03 14:23:59 +0100

    add row pokemonList and change program icon

---
+++ app/src/main/java/com/example/apppokedex/models/PokedexListEntry.kt
@@ -0,0 +1,8 @@
+package com.example.apppokedex.models
+
+class PokedexListEntry (
+    val pokemonName: String,
+    val imageUrl: String,
+    val number: Int
+        ){
+}
--- app/src/main/java/com/example/apppokedex/pokemonList/PokemonListScreen.kt
+++ app/src/main/java/com/example/apppokedex/pokemonList/PokemonListScreen.kt
@@ -2,9 +2,12 @@ package com.example.apppokedex.pokemonList
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,19 +15,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.FocusOrder
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.request.ImageRequest
 import com.example.apppokedex.R
+import com.example.apppokedex.models.PokedexListEntry
+import com.example.apppokedex.ui.theme.Roboto
+import com.google.accompanist.coil.CoilImage
 
 @Composable
 fun PokemonListScreen(
@@ -38,7 +54,9 @@ fun PokemonListScreen(
             Spacer(modifier = Modifier.height(20.dp))
             Image(painter = painterResource(id = R.mipmap.ic_internation_pok_mon_logo),
                 contentDescription = "Pokemon",
-                modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally) //todo size
                 )
                 SearchBar(
                     hint = "Search",
@@ -90,5 +108,98 @@ fun SearchBar(
                     .padding(20.dp,12.dp)
             )
         }
+    }
+}
+
+@Composable
+fun PokedexEntery(
+    entry: PokedexListEntry,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    viewModel: PokemonListViewModel = hiltViewModel()
+) {
+    val defaultDominantColor = MaterialTheme.colors.surface
+    var dominantColor by remember {
+        mutableStateOf(defaultDominantColor)
+    }
+
+    Box(
+      contentAlignment = Alignment.Center,
+      modifier = Modifier
+          .shadow(5.dp, RoundedCornerShape(10.dp))
+          .clip(RoundedCornerShape(10.dp))
+          .aspectRatio(1f)
+          .background(
+              Brush.verticalGradient(
+                  listOf(
+                      dominantColor,
+                      defaultDominantColor
+                  )
+              )
+          )
+          .clickable {
+              navController.navigate(
+                  "pokemon_detail_screen/${dominantColor.toArgb()}/${entry.pokemonName}"
+              )
+          }
+    ){
+        Column {
+            CoilImage(
+                request = ImageRequest.Builder(LocalContext.current)
+                    .data(entry.imageUrl)
+                    .target
+                    {
+                        viewModel.calcDominantColor(it){ color ->
+                            dominantColor = color
+                        }
+                    }
+                    .build(),
+                contentDescription = entry.pokemonName,
+                fadeIn = true,
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(CenterHorizontally)
+            ) {
+                CircularProgressIndicator(
+                    color = MaterialTheme.colors.primary,
+                    modifier = Modifier.scale(0.5f)
+                )
+                Text(
+                    text = entry.pokemonName,
+                    fontFamily = Roboto,
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun PokeRow(
+    rowIndex: Int,
+    entries: List<PokedexListEntry>,
+    navController: NavController
+) {
+    Column {
+        Row{
+            PokedexEntery(
+                entry = entries[rowIndex * 2],
+                navController = navController,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            if(entries.size >= rowIndex * 2 + 2){
+                PokedexEntery(
+                    entry = entries[rowIndex * 2 + 1],
+                    navController = navController,
+                    modifier = Modifier.weight(1f)
+                )
+            } else{
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
